0d267f4b2def70d102c6618cff8bdaa6ae8384c4
sung min song <song47166@naver.com>
2024-05-16 19:06:10 +0900

    :white_check_mark: 클라우드 스토리지 버킷 연결 방식 변경

--- src/main/java/com/example/SoftwareEngineering_Project/Config/GoogleCloudStorageConfig.java
+++ src/main/java/com/example/SoftwareEngineering_Project/Config/GoogleCloudStorageConfig.java
@@ -6,7 +6,6 @@ import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
