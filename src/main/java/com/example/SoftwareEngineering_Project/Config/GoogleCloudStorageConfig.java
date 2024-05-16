2ec5a0ec0f419d3a9e5e888bbe4586125049275b
sung min song <song47166@naver.com>
2024-05-16 18:57:44 +0900

    :white_check_mark: 클라우드 스토리지 버킷 연결 방식 변경

--- src/main/java/com/example/SoftwareEngineering_Project/Config/GoogleCloudStorageConfig.java
+++ src/main/java/com/example/SoftwareEngineering_Project/Config/GoogleCloudStorageConfig.java
@@ -3,25 +3,25 @@ package com.example.SoftwareEngineering_Project.Config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.Base64;
 
 @Configuration
 public class GoogleCloudStorageConfig {
 
+    @Value("${google.cloud.credentials.base64key}")
+    private String base64Key;
+
     @Bean
     public Storage storage() throws IOException {
-
-        ClassPathResource resource = new ClassPathResource("rock-palisade-406504-30f9e0518c74.json");
-        GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
-        String projectId = "rock-palisade-406504";
-        return StorageOptions.newBuilder()
-                .setProjectId(projectId)
-                .setCredentials(credentials)
-                .build()
-                .getService();
+        byte[] decodedKey = Base64.getDecoder().decode(base64Key);
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new ByteArrayInputStream(decodedKey));
+        return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
     }
 }
